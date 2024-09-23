6aa09845ee410f1dad06f4f8b921693b66a82c92
serii burduja <seriiburduja@gmail.com>
2024-09-23 12:37:41 +0300

    feat: print command

--- buffer.py
+++ buffer.py
@@ -2,13 +2,15 @@ import os
 
 def addToClipBoard(text):
     command = 'echo ' + text.strip() + '| xclip -selection clipboard'
+    print(f"command: {command}")
     os.system(command)
 
 def addToClipBoardFile(file):
     command = f"cat {file} | xclip -selection clipboard"
-    # print(command)
+    print(command)
     os.system(command)
 
 def getFromClipBoard():
     command = 'xclip -o -selection clipboard'
+    print(f"command: {command}")
     return os.popen(command).read().strip()
